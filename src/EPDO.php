797c866e7295620ade8e1b7c770e209a79de3c0e
DarthCorvidus <github@vm01.telton.de>
2021-08-03 14:28:56 +0200

    Initial commit.

---
+++ src/EPDO.php
@@ -0,0 +1,158 @@
+<?php
+/**
+ * @copyright (c) 2021, Claus-Christoph Küthe
+ * @author Claus-Christoph Küthe <floss@vm01.telton.de>
+ * @license LGPL
+ */
+
+/**
+ * EPDO
+ * 
+ * Enhancement for EPDO with functions which will save a lot of time when
+ * writing CRUD applications by providing methods to Create, Read, Update and
+ * Delete rows.
+ * Values/Conditions are to be provided as associative arrays with the array
+ * keys corresponding to column names. Please not NOT TO USE KEYS FROM UNTRUSTED
+ * SOURCES, e.g. $values[$_GET["column1"]]. Although methods use bound values,
+ * column names could be used for SQL injection. 
+ */
+class EPDO extends PDO {
+	/**
+	 * create
+	 * 
+	 * Creates a new entry from an associative array, where key names are
+	 * columns. Do not use key names from untrusted sources.
+	 * 
+	 * @param string $table
+	 * @param array $array Associative array containing columns as keys and values. DO NOT USE KEYS FROM UNTRUSTED SOURCES!
+	 * @param string $sequence Sequence name where needed (eg. PostgreSQL)
+	 * @return int Insert Id
+	 */
+	function create(string $table, array $array, string $sequence = NULL): int {
+		$names = array();
+		$params = array();
+		$bind = array();
+		$insert = "INSERT INTO ".$this->quote($table)." ";
+		foreach($array as $key => $value) {
+			if($value===NULL) {
+				continue;
+			}
+			$names[] = $key;
+			$params[] = $value;
+			$bind[] = "?";
+		}
+		$insert .= "(".implode(", ", $names).") VALUES (".implode(", ", $bind).")";
+		$stmt = $this->prepare($insert);
+		$stmt->execute($params);
+	return $this->lastInsertId($sequence);
+	}
+	
+	/**
+	 * update
+	 * 
+	 * Updates a value. Use it for simple updates like changing one value.
+	 * Both colums to be changed and the conditions are expected as associative
+	 * arrays, where the array keys are columns. Conditions are joined by AND.
+	 * 
+	 * @param string $table Table name
+	 * @param array $values Associative array. DO NOT USE KEY FROM UNTRUSTED SOURCES!
+	 * @param array $conditions Associative array. DO NOT USE KEY FROM UNTRUSTED SOURCES!
+	 */
+	function update(string $table, array $values, array $conditions) {
+		$update = "UPDATE ".$this->quote($table)." SET ";
+		$set = array();
+		foreach($values as $key => $value) {
+			$set[] = $key." = ?";
+			$param[] = $value;
+		}
+		$update .= implode(", ", $set)." WHERE ";
+		foreach($conditions as $key => $value) {
+			if($value===NULL) {
+				$where[] = $key." IS NULL";
+			continue;
+			}
+			$where[] = $key." = ?";
+			$param[] = $value;
+		}
+		$update .= implode($where, " AND ");
+		$stmt = $this->prepare($update);
+		$stmt->execute($param);
+	}
+	
+	/**
+	 * delete
+	 * 
+	 * Delete values using associative array as condition with column names as
+	 * keys. Multiple conditions are linked via AND. Be careful, and no don use
+	 * keys from untrusted sources.
+	 * @param string $table Table name
+	 * @param array $conditions Condition as associative array. DO NOT USE KEYS FROM UNTRUSTED SOURCES!
+	 */
+	function delete(string $table, array $conditions) {
+		$param = array();
+		$where = array();
+		$delete = "DELETE FROM ".$this->quote($table)." WHERE ";
+		foreach($conditions as $key => $value) {
+			if($value===NULL) {
+				$where[] = $key." IS NULL";
+			continue;
+			}
+			$where[] = $key." = ?";
+			$param[] = $value;
+		}
+		$delete .= implode($where, " AND ");
+		$stmt = $this->prepare($delete);
+		$stmt->execute($param);
+	}
+	
+	/**
+	 * row
+	 * 
+	 * Shorthand to select a single row. The intention for row is to retrieve
+	 * one single dataset, so if you're getting more than one result, an 
+	 * Exception is thrown, as I'd assume something is wrong with my query or
+	 * database design or data. Can be mitigated with LIMIT 1.
+	 * 
+	 * @param string $sql
+	 * @param array $params
+	 * @return array
+	 * @throws PDOException
+	 */
+	function row(string $sql, array $params): array {
+		$stmt = $this->prepare($sql);
+		$stmt->execute($params);
+		$stmt->setFetchMode(PDO::FETCH_ASSOC);
+		$fetch = $stmt->fetchAll();
+		if(count($fetch)===0) {
+			return array();
+		}
+		if(count($fetch)!==1) {
+			throw new PDOException("Result is ambiguous (".count($fetch).")");
+		}
+	return $fetch[0];
+	}
+	
+	/**
+	 * result
+	 * 
+	 * Shorthand to return a single value; throws an exception if receiving more
+	 * than one row.
+	 * @param string $sql
+	 * @param array $params
+	 * @return type
+	 * @throws PDOException
+	 */
+	function result(string $sql, array $params) {
+		$stmt = $this->prepare($sql);
+		$stmt->execute($params);
+		$stmt->setFetchMode(PDO::FETCH_NUM);
+		$fetch = $stmt->fetchAll();
+		if(empty($fetch[0])) {
+			return NULL;
+		}
+		if(count($fetch)!==1) {
+			throw new PDOException("Result is ambiguous (".count($fetch).")");
+		}
+	return $fetch[0][0];
+	}
+}
---
+++ tests/EPDOTest.php
@@ -0,0 +1,200 @@
+<?php
+declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of AssertTest
+ *
+ * @author hm
+ */
+class EPDOTest extends TestCase {
+	function setUp() {
+		copy(__DIR__."/epdo.sqlite", __DIR__."/testing.sqlite");
+	}
+	
+	function tearDown() {
+		#unlink(__DIR__."/testing.sqlite");
+	}
+	
+	function getEPDO(): EPDO {
+		$pdo = new EPDO("sqlite:".__DIR__."/testing.sqlite", "", "");
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	return $pdo;
+	}
+
+	function getValues(): array {
+		$pdo = new EPDO("sqlite:".__DIR__."/testing.sqlite", "", "");
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$stmt = $pdo->prepare("select * from politician order by id");
+		$stmt->setFetchMode(EPDO::FETCH_ASSOC);
+		$stmt->execute();
+		$results = array();
+		foreach($stmt as $value) {
+			$results[] = $value;
+		}
+	return $results;
+	}
+	
+	
+	function getExpected() {
+		$values[0]["id"] = "1";
+		$values[0]["name"] = "Angela";
+		$values[0]["surname"] = "Merkel";
+		$values[0]["sex"] = "f";
+		$values[0]["birthday"] = "1954-07-17";
+
+		$values[1]["id"] = "2";
+		$values[1]["name"] = "Emmanuel Jean-Michael Frédéric";
+		$values[1]["surname"] = "Macron";
+		$values[1]["sex"] = "f";
+		$values[1]["birthday"] = "1977-12-21";
+
+		$values[2]["id"] = "3";
+		$values[2]["name"] = "Alexander Boris";
+		$values[2]["surname"] = "de Pfeffel Johnson";
+		$values[2]["sex"] = "m";
+		$values[2]["birthday"] = "1964-06-19";
+
+		$values[3]["id"] = "4";
+		$values[3]["name"] = "Joseph Robinette";
+		$values[3]["surname"] = "Biden";
+		$values[3]["sex"] = "m";
+		$values[3]["birthday"] = "1942-11-20";
+
+	return $values;
+	}
+	
+	function testSelect() {
+		$this->assertEquals($this->getExpected(), $this->getValues());
+	}
+	
+	function testInsert() {
+		$insert["name"] = "Vladimir Vladimirovich";
+		$insert["surname"] = "Putin";
+		$insert["sex"] = 'm';
+		$insert["birthday"] = '1952-10-07';
+		$pdo = $this->getEPDO();
+		$insertId = $pdo->create("politician", $insert);
+		$insert["id"] = 5;
+		$expected = $this->getExpected();
+		$expected[] = $insert;
+		$this->assertEquals($expected, $this->getValues());
+		$this->assertEquals(5, $insertId);
+	}
+	
+	function testUpdate() {
+		$expected = $this->getExpected();
+		$expected[1]['sex'] = 'm';
+		$this->getEPDO()->update("politician", array('sex' => 'm'), array('id' => 2, 'sex' => 'f'));
+		$values = $this->getValues();
+		$this->assertEquals($expected, $values);
+	}
+	
+	function testUpdateValueNULL() {
+		$expected = $this->getExpected();
+		$expected[1]['sex'] = NULL;
+		$this->getEPDO()->update("politician", array('sex' => NULL), array('id' => 2, 'sex' => 'f'));
+		$values = $this->getValues();
+		$this->assertEquals($expected, $values);
+		$this->assertEquals(true, ($expected[1]["sex"]===NULL));
+	}
+
+	function testUpdateValueEmpty() {
+		$expected = $this->getExpected();
+		$expected[1]['sex'] = "";
+		$this->getEPDO()->update("politician", array('sex' => ""), array('id' => 2, 'sex' => 'f'));
+		$values = $this->getValues();
+		$this->assertEquals($expected, $values);
+		$this->assertEquals(true, ($expected[1]["sex"]===""));
+	}
+	
+	function testUpdateConditionNULL() {
+		$insert["name"] = "Vladimir Vladimirovich";
+		$insert["surname"] = "Putin";
+		$insert["sex"] = 'm';
+		
+		$pdo = $this->getEPDO();
+		$pdo->create("politician", $insert);
+		
+		$insert["id"] = "5";
+		$insert["birthday"] = NULL;
+		
+		$expected = $this->getExpected();
+		$expected[] = $insert;
+		
+		$this->assertEquals($expected, $this->getValues());
+		
+		$this->getEPDO()->update("politician", array('birthday' => "1952-10-07"), array('id' => 5, 'birthday' => NULL));
+		$expected[4]["birthday"] = "1952-10-07";
+		
+		$this->assertEquals($expected, $this->getValues());
+	}
+	
+	function testUpdateConditionEmpty() {
+		$insert["name"] = "Vladimir Vladimirovich";
+		$insert["surname"] = "Putin";
+		$insert["sex"] = 'm';
+		$insert["birthday"] = "";
+		
+		$pdo = $this->getEPDO();
+		$pdo->create("politician", $insert);
+		$insert["id"] = "5";
+		$insert["birthday"] = "";
+		
+		$expected = $this->getExpected();
+		$expected[] = $insert;
+		
+		$this->assertEquals($expected, $this->getValues());
+		
+		$this->getEPDO()->update("politician", array('birthday' => "1952-10-07"), array('id' => 5, 'birthday' => ""));
+		$expected[4]["birthday"] = "1952-10-07";
+		
+		$this->assertEquals($expected, $this->getValues());
+	}
+	
+	function testDelete() {
+		$expected = $this->getExpected();
+		unset($expected[3]);
+		$this->getEPDO()->delete("politician", array("id"=>4));
+		$this->assertEquals($expected, $this->getValues());
+	}
+	
+	function testRead() {
+		$expected = $this->getExpected();
+		$read = $this->getEPDO()->read("select * from politician where id = ?", array(1));
+		$this->assertEquals($expected[0], $read);
+	}
+	
+	function testReadAmbiguous() {
+		$expected = $this->getExpected();
+		$this->expectException(PDOException::class);
+		$this->expectExceptionMessage("Result is ambiguous (2)");
+		$read = $this->getEPDO()->read("select * from politician where sex = ?", array("m"));
+	}
+
+	function testReadEmpty() {
+		$read = $this->getEPDO()->read("select * from politician where id = ?", array(10));
+		$this->assertEquals(array(), $read);
+	}
+
+	function testResult() {
+		$result = $this->getEPDO()->result("select count(*) from politician where sex = ?", array('f'));
+		$this->assertEquals(2, $result);
+	}
+	
+	function testResultNoValue() {
+		$result = $this->getEPDO()->result("select name from politician where id = ?", array('15'));
+		$this->assertEquals(0, $result);
+	}
+
+	function testResultAmbiguous() {
+		$this->expectException(PDOException::class);
+		$this->expectExceptionMessage("Result is ambiguous (2)");
+		$result = $this->getEPDO()->result("select name from politician where sex = ?", array('f'));
+	}
+}
